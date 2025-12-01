6315e0cb0fec5429dcf9332b734d7772fd49d3c8
Ethan Lee <LeeEthan@JohnDeere.com>
2025-12-01 08:34:40 -0600

    feat: wrap 2024 and start 2025

--- 2024/1/algorithm.ts
+++ 2024/1/algorithm.ts
@@ -1,13 +1,28 @@
 import data from "./data.json";
 
-const first = data[0]!.sort();
-const second = data[1]!.sort();
+const partOne = () => {
+  const first = data[0]!.sort();
+  const second = data[1]!.sort();
+  // part 1
+  console.log(
+    first.reduce(
+      (previous, current, currentIndex) =>
+        Math.abs(current! - second[currentIndex]!) + previous,
+      0,
+    ),
+  );
+};
 
-// part 1
-console.log(
-  first.reduce(
-    (previous, current, currentIndex) =>
-      Math.abs(current! - second[currentIndex]!) + previous,
-    0,
-  ),
-);
+// part 2
+// combine the two arrays, sort it, stop adding the number when the number changes O((n+m)^3)
+const partTwo = () => {
+  console.log(
+    new Set(data[0])
+      .intersection(new Set(data[1]))
+      .values()
+      .reduce((previous, current) => previous + current, 0),
+  );
+};
+
+partOne();
+partTwo();
