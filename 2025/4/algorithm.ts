623434f9e289112d1729801631293828cd707066
Ethan Lee <LeeEthan@JohnDeere.com>
2025-12-04 10:53:41 -0600

    4-2

--- 2025/4/algorithm.ts
+++ 2025/4/algorithm.ts
@@ -40,10 +40,54 @@ const algorithmOne = () => {
 };
 
 const algorithmTwo = () => {
-  const aggregate = data.reduce((aggregator, current) => {
-    return aggregator;
-  }, 0);
+  const pullAndCount = (data: string[][]) => {
+    return data.reduce((pullableRolls, shelf, shelfIndex) => {
+      shelf.forEach((slot, slotIndex) => {
+        if (slot === "@") {
+          let numSiblings = 0;
+          if (data[shelfIndex - 1] && data[shelfIndex - 1]![slotIndex - 1] === "@") {
+            numSiblings++;
+          }
+          if (data[shelfIndex - 1] && data[shelfIndex - 1]![slotIndex] === "@") {
+            numSiblings++;
+          }
+          if (data[shelfIndex - 1] && data[shelfIndex - 1]![slotIndex + 1] === "@") {
+            numSiblings++;
+          }
+          if (data[shelfIndex]![slotIndex - 1] === "@") {
+            numSiblings++;
+          }
+          if (data[shelfIndex]![slotIndex + 1] === "@") {
+            numSiblings++;
+          }
+          if (data[shelfIndex + 1] && data[shelfIndex + 1]![slotIndex - 1] === "@") {
+            numSiblings++;
+          }
+          if (data[shelfIndex + 1] && data[shelfIndex + 1]![slotIndex] === "@") {
+            numSiblings++;
+          }
+          if (data[shelfIndex + 1] && data[shelfIndex + 1]![slotIndex + 1] === "@") {
+            numSiblings++;
+          }
+          if (numSiblings < 4) {
+            data[shelfIndex]![slotIndex] = ".";
+            return pullableRolls++;
+          }
+        }
+      });
+      return pullableRolls;
+    }, 0);
+  };
+  let aggregate = 0;
+  while (true) {
+    const result = pullAndCount(data);
+    if (result === 0) {
+      break;
+    } else {
+      aggregate += result;
+    }
+  }
   console.log("solution", aggregate);
 };
 
-algorithmOne();
+algorithmTwo();
